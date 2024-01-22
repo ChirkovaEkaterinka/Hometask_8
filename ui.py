552c32ed019708bd4a455674703c2a6c0013905e
ChirkovaEkaterinka <108570867+ChirkovaEkaterinka@users.noreply.github.com>
2024-01-23 00:01:56 +0300

    Add files via upload

---
+++ ui.py
@@ -0,0 +1,20 @@
+from logger import input_data, print_data
+from added_function import edit_data, delete_data
+from csv import *
+
+def interface():
+    print('Добрый день! Вы попали на бот-справочник от GeekBrains! \n 1 - запись данных \n 2 - вывод данных \n 3 - изменение данных \n 4 - удаление данных \n')
+    command = int(input('Введите число '))
+
+    while command != 1 and command != 2 and command != 3 and command != 4:
+        print('Неправильный ввод')
+        command = int(input('Введите число '))    
+    
+    if command == 1:
+        input_data()
+    elif command == 2:
+        print_data()
+    elif command == 3:
+        edit_data()
+    elif command == 4:
+        delete_data()
